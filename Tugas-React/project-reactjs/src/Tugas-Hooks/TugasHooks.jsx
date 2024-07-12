90665717cf7ca5ee744e2c3eccc7023c3de4152c
Artzy1401 <afifarsyi@gmail.com>
2024-07-12 10:06:49 +0700

    Add Tugas Hooks with Component Lifcycle

--- Tugas-React/project-reactjs/src/Tugas-Hooks/TugasHooks.jsx
+++ Tugas-React/project-reactjs/src/Tugas-Hooks/TugasHooks.jsx
@@ -1,28 +1,76 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-function TugasHooks() {
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
-  const [countdown, setCountdown] = useState(100);
+// function TugasHooks() {
+//   const [time, setTime] = useState(new Date().toLocaleTimeString());
+//   const [countdown, setCountdown] = useState(100);
 
-  useEffect(() => {
-    const intervalID = setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-      setCountdown((prevCountdown) => (prevCountdown > 0 ? prevCountdown - 1 : 0));
-    }, 1000);
+//   useEffect(() => {
+//     const intervalID = setInterval(() => {
+//       setTime(new Date().toLocaleTimeString());
+//       setCountdown((prevCountdown) => (prevCountdown > 0 ? prevCountdown - 1 : 0));
+//     }, 1000);
 
-    return () => clearInterval(intervalID);
-  }, []);
+//     return () => clearInterval(intervalID);
+//   }, []);
 
-  if (countdown === 0) {
-    return null;
+//   if (countdown === 0) {
+//     return null;
+//   }
+
+//   return (
+//     <div className="container-hooks">
+//       <h1>Now At - {time}</h1>
+//       <p>Countdown: {countdown}</p>
+//     </div>
+//   );
+// }
+
+// export default TugasHooks;
+import React, { Component } from "react";
+
+class TugasHooks extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+      countdown: 100,
+    };
   }
 
-  return (
-    <div className="container-hooks">
-      <h1>Now At - {time}</h1>
-      <p>Countdown: {countdown}</p>
-    </div>
-  );
+  componentDidMount() {
+    const intervalID = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+        countdown: this.state.countdown > 0 ? this.state.countdown - 1 : 0,
+      });
+    }, 1000);
+
+    if (this.state.countdown === 0) {
+      clearInterval(intervalID);
+    }
+  } 
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  render() {
+    const { time, countdown } = this.state;
+
+    if (countdown === 0) {
+      return null;
+    }
+
+    return (
+      <>
+      <div className="container-hooks">
+        <h1>Now At - {time}</h1>
+        <p>Countdown: {countdown}</p>
+      </div>
+      </>
+  
+    )
+  }
 }
 
-export default TugasHooks;
+export default TugasHooks
