5be7bca88aaf9c8a53d3c2bddf5fcc1ba0657826
Artzy1401 <afifarsyi@gmail.com>
2024-07-31 11:35:48 +0700

    Update dosen handler at GetAl function

--- Tugas-React/backend-tugas-reactjs/app/models/dosen.go
+++ Tugas-React/backend-tugas-reactjs/app/models/dosen.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Dosen struct {
-	ID           int          `gorm:"primaryKey" json:"id"`
-	Nama         string       `gorm:"type:varchar(255)" json:"nama" validate:"required"`
-	CreatedAt    time.Time    `json:"created_at"`
-	UpdatedAt    time.Time    `json:"updated_at"`
-	MataKuliahID int          `json:"matakuliah_id"`
-	MataKuliah   []MataKuliah `json:"matakuliah" gorm:"foreignKey:MataKuliahID; manyToMany:MataKuliah;"`
+	ID           int        `gorm:"primaryKey" json:"id"`
+	Nama         string     `gorm:"type:varchar(255)" json:"nama" validate:"required"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    time.Time  `json:"updated_at"`
+	MataKuliahID int        `json:"matakuliah_id"`
+	MataKuliah   MataKuliah `gorm:"foreignKey:MataKuliahID; manyToMany:MataKuliah;" json:"matakuliah"`
 }
